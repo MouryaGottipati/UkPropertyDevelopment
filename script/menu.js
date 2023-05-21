0f52d80affb152ca50c70e376b00b312900ffff2
Madhu <madhummr2000@gmail.com>
2023-05-21 21:58:53 +0530

    Checking for removing white space when menu is closed in mobile view

--- script/menu.js
+++ script/menu.js
@@ -71,7 +71,7 @@ if (screenWidth <= 767) {
             // Perform actions when the class is present
             menuIcon.classList.remove('open');
             menu.style.display = 'none';
-            main.style.marginBottom = 75 + 'px';
+            main.style.marginBottom = 50 + 'px';
 
 
         } else {
@@ -79,7 +79,7 @@ if (screenWidth <= 767) {
             // Perform actions when the class is not present
             menuIcon.classList.toggle('open');
             menu.style.display = 'block';
-            main.style.marginBottom = 150 + 'px';
+            main.style.marginBottom = 140 + 'px';
 
         }
 
