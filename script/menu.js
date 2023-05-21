05438c12fcec833391590d1fe6c83dd2c3cbe2d0
Madhu <madhummr2000@gmail.com>
2023-05-21 21:44:53 +0530

    Checking for removing white space when menu is closed in mobile view

--- script/menu.js
+++ script/menu.js
@@ -4,94 +4,97 @@ const sections = Array.from(document.querySelectorAll('.content section'));
 
 // Hide all sections except for the home section
 sections.forEach(section => {
-  if (section.className !== 'home') {
-    section.style.display = 'none';
-  }
+    if (section.className !== 'home') {
+        section.style.display = 'none';
+    }
 });
 
 menuItems.forEach(menuItem => {
-  menuItem.addEventListener('click', function () {
-    const sectionId = this.id;
+    menuItem.addEventListener('click', function() {
+        const sectionId = this.id;
 
-    const section = document.querySelector('.' + sectionId);
+        const section = document.querySelector('.' + sectionId);
 
-    if (section) {
-      // Hide all sections except for the selected section
-      sections.forEach(otherSection => {
-        if (otherSection !== section) {
-          otherSection.style.display = 'none';
-        }
-      });
-
-      section.style.display = 'block';
-
-      // Check if the selected menu item has a submenu
-      const submenu = this.querySelector('.submenu');
-      if (submenu) {
-
-        submenuItems.forEach(submenuItem => {
-          submenuItem.addEventListener('click', function (event) {
-            event.stopPropagation();
-            const subSectionId = this.id;
-            const subSection = document.querySelector('.' + subSectionId);
-            if (subSection) {
-              // Hide all sections except for the selected section
-              sections.forEach(otherSection => {
-                if (otherSection !== subSection) {
-                  otherSection.style.display = 'none';
+        if (section) {
+            // Hide all sections except for the selected section
+            sections.forEach(otherSection => {
+                if (otherSection !== section) {
+                    otherSection.style.display = 'none';
                 }
-              });
+            });
 
-              subSection.style.display = 'block';
+            section.style.display = 'block';
+
+            // Check if the selected menu item has a submenu
+            const submenu = this.querySelector('.submenu');
+            if (submenu) {
+
+                submenuItems.forEach(submenuItem => {
+                    submenuItem.addEventListener('click', function(event) {
+                        event.stopPropagation();
+                        const subSectionId = this.id;
+                        const subSection = document.querySelector('.' + subSectionId);
+                        if (subSection) {
+                            // Hide all sections except for the selected section
+                            sections.forEach(otherSection => {
+                                if (otherSection !== subSection) {
+                                    otherSection.style.display = 'none';
+                                }
+                            });
+
+                            subSection.style.display = 'block';
+                        }
+                    });
+                });
             }
-          });
-        });
-      }
-    }
-  });
+        }
+    });
 });
 
 const screenWidth = window.innerWidth;
 if (screenWidth <= 767) {
-  const menuIcon = document.getElementById('menu-icon');
-  const menu = document.getElementById('menu');
-  menuIcon.addEventListener('click', handleMenuIconClick);  
+    const menuIcon = document.getElementById('menu-icon');
+    const menu = document.getElementById('menu');
+    menuIcon.addEventListener('click', handleMenuIconClick);
+    const main = document.getElementById('main');
+    // Function to handle menu icon click
+    function handleMenuIconClick() {
+        // if (menu.style.getPropertyValue('display').toString().toLowerCase() !== 'none') {
 
-  // Function to handle menu icon click
-  function handleMenuIconClick() {
-    // if (menu.style.getPropertyValue('display').toString().toLowerCase() !== 'none') {
-      
-    // }
-    // else {
-      
-    // }
-    
-    if (menuIcon.classList.contains('open')) {
-      // The 'open' class is present
-      // Perform actions when the class is present
-      menuIcon.classList.remove('open');
-      menu.style.display = 'none';
-      
-    } else {
-      // The 'open' class is not present
-      // Perform actions when the class is not present
-      menuIcon.classList.toggle('open');
-      menu.style.display = 'block';
-      
+        // }
+        // else {
+
+        // }
+
+        if (menuIcon.classList.contains('open')) {
+            // The 'open' class is present
+            // Perform actions when the class is present
+            menuIcon.classList.remove('open');
+            menu.style.display = 'none';
+            main.style.marginBottom = 150 + 'px';
+
+
+        } else {
+            // The 'open' class is not present
+            // Perform actions when the class is not present
+            menuIcon.classList.toggle('open');
+            menu.style.display = 'block';
+            main.style.marginBottom = 250 + 'px';
+
+        }
+
     }
 
-  }
+    // // Function to handle menu item click
+    // function handleMenuItemClick() {
+    //   menuIcon.classList.remove('open');
 
-  // // Function to handle menu item click
-  // function handleMenuItemClick() {
-  //   menuIcon.classList.remove('open');
-    
-  //   menu.style.display = 'none';
-  // }
+    //   menu.style.display = 'none';
+    // }
 
-  // Attach click event listeners
- 
-  // menuItems.forEach(menuItem => {
-  //   menuItem.addEventListener('click', handleMenuItemClick);
-  // });
+    // Attach click event listeners
+
+    // menuItems.forEach(menuItem => {
+    //   menuItem.addEventListener('click', handleMenuItemClick);
+    // });
 }
